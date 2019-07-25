6e2553f10f5af5dc72144cb0f133753cc12ccc46
Josh Stockin <joshstockin@gmail.com>
2019-07-25 17:12:47 -0500

    Add site Start logic for GET requests

--- src/python/Site.py
+++ src/python/Site.py
@@ -20,29 +20,65 @@
 # You should have received a copy of the GNU General Public License
 # along with Profess. If not, see <https://www.gnu.org/licenses/>.
 
-from types import *
-from SiteConfig import *
-from Model import *
-from View import *
-from Controller import *
+from .types import *
+from .SiteConfig import *
+from .Model import *
+from .View import *
+from .Controller import *
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+def request(requestType, siteObj, requestObj):
+	pass
 
 class Site:
 	__config = None
 	__running = False
 	__server = None
+	__site = None
 
 	def __init__(self, config):
 		types(SiteConfig, config)
 		self.__config = config
-		return self
+		self.__site = self
 
-	def Start():
-		types(SiteConfig, __config)
+	def Start(self):
+		types(SiteConfig, self.__config)
+		if self.__running:
+			raise Exception("Server is already running")
 
-	def Stop():
-		if not __running:
+		class RequestHandler(BaseHTTPRequestHandler):
+			def do_GET(self):
+				view = None
+				if not view:
+					self.send_response(404)
+					self.end_headers()
+					self.wfile.write(b'Error 404')
+					return
+
+				self.send_response(200)
+				self.end_headers()
+				print(self.command)
+				print(self.path)
+				self.wfile.write(b'<body><h1>hello world</h1></body>')
+		RequestHandler.siteObj = self
+
+		self.__server = HTTPServer(('localhost', self.__config.Port), RequestHandler)
+
+		if self.__config.SSL_Enabled:
+			pass
+
+		self.__server.serve_forever()
+
+	def Stop(self):
+		if not self.__running:
 			return True
-		pass
+		try:
+			__server.shutdown()
+			__server.server_close()
+			self.__running = false
+			return True
+		except:
+			raise Exception("Failed to shut active server socket down")
 
 
 	def AddController(self, controller):
